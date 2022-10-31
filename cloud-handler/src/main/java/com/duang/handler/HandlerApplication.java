e9b5e15fdf2fafd732e4073d2f25119b14d050da
dongwei <598644648@qq.com>
2022-10-31 15:50:57 +0800

    使用sentinel 服务容错(没完成)

--- cloud-handler/src/main/java/com/duang/handler/HandlerApplication.java
+++ cloud-handler/src/main/java/com/duang/handler/HandlerApplication.java
@@ -5,10 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan({"com.duang.handler.mapper"})
 @EnableDiscoveryClient
+@EnableFeignClients //开启Fegin
 public class HandlerApplication {
 
     public static void main(String[] args) {
--- cloud-web/src/main/java/com/duang/web/feign/HandlerFeign.java
+++ cloud-web/src/main/java/com/duang/web/feign/HandlerFeign.java
@@ -2,7 +2,6 @@ package com.duang.web.feign;
 
 import com.duang.cloudcommons.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
